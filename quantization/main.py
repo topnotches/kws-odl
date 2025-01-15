2d214a74e60856a413b9a418218186f41461825f
Jesper Villaluz Dalgaard (JPDG) <jpdg@demant.com>
2025-01-15 11:52:39 +0100

    added model param export to kws offline part

--- quantization/main.py
+++ quantization/main.py
@@ -1,86 +1,112 @@
-# Copyright (C) 2021 ETH Zurich
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# SPDX-License-Identifier: Apache-2.0
-# ==============================================================================
-#
-# Author: Cristian Cioflan, ETH (cioflanc@iis.ee.ethz.ch)
-
+STEP_DO_TRAIN           = False
+STEP_DO_EXPORT_MODEL    = True
+CHECKPOINT_PATH         = './model_acc_94.53125.pth'
+CLASSES                 = 12
+EXPORT_OUTPUT_DIR_PATH  = '../simulation/exported_models/'
+EXPORT_OUTPUT_NAME      = 'export_params_nclass_' + str(CLASSES) + '.pth'
+EXPORT_OUTPUT_PATH      = EXPORT_OUTPUT_DIR_PATH+EXPORT_OUTPUT_NAME
 
 import torch
 import dataset
 import os
 import time
 import math
-#import nemo
-
 from torchsummary import summary
 from model import DSCNN
 from utils import remove_txt, parameter_generation
 from copy import deepcopy
 from pthflops import count_ops
+import csv
 from train import Train
-
+import shutil
 
 # Device setup
 if torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
-print (torch.version.__version__)
+print(torch.version.__version__)
 print(device)
 
-# Parameter generation
-training_parameters, data_processing_parameters = parameter_generation()  # To be parametrized
-
-# Dataset generation
-audio_processor = dataset.AudioProcessor(training_parameters, data_processing_parameters)
-
-train_size = audio_processor.get_size('training')
-valid_size = audio_processor.get_size('validation')
-test_size = audio_processor.get_size('testing')
-print("Dataset split (Train/valid/test): "+ str(train_size) +"/"+str(valid_size) + "/" + str(test_size))
-
-# Model generation and analysis
-model = DSCNN(use_bias = True)
+# Model generation
+model = DSCNN(use_bias=True)
 model.to(device)
-summary(model,(1,49,data_processing_parameters['feature_bin_count']))
-dummy_input = torch.rand(1, 1,49,data_processing_parameters['feature_bin_count']).to(device)
-count_ops(model, dummy_input)
 
-# Training initialization
-trainining_environment = Train(audio_processor, training_parameters, model, device)
 
-# Removing stored inputs and activations
-remove_txt()
+if STEP_DO_TRAIN:
+    
+    # Parameter generation
+    training_parameters, data_processing_parameters = parameter_generation()  # To be parametrized
 
-start=time.clock_gettime(0)
-trainining_environment.train(model)
-print('Finished Training on GPU in {:.2f} seconds'.format(time.clock_gettime(0)-start))
+    # Dataset generation
+    audio_processor = dataset.AudioProcessor(training_parameters, data_processing_parameters)
 
-# Ignoring training, load pretrained model
-model.load_state_dict(torch.load('./model.pth', map_location=torch.device('cuda')))
+    train_size = audio_processor.get_size('training')
+    valid_size = audio_processor.get_size('validation')
+    test_size = audio_processor.get_size('testing')
+    print("Dataset split (Train/valid/test): " + str(train_size) + "/" + str(valid_size) + "/" + str(test_size))
 
-# Accuracy on the training set. 
-# # print ("Training acc")
-# acc = trainining_environment.validate(model, mode='training', batch_size=-1, statistics=False)
-# # Accuracy on the validation set. 
-# print ("Validation acc")
-# acc = trainining_environment.validate(model, mode='validation', batch_size=-1, statistics=False)
-# # Accuracy on the testing set. 
-# print ("Testing acc")
-# acc = trainining_environment.validate(model, mode='testing', batch_size=-1, statistics=False)
+    # Model analysis
+    print("Printing model summary...")
+    summary(model, (1, 49, data_processing_parameters['feature_bin_count']))
+    dummy_input = torch.rand(1, 1, 49, data_processing_parameters['feature_bin_count']).to(device)
+    count_ops(model, dummy_input)
+    # Training initialization
+    print("Initializing training environment...")
+    trainining_environment = Train(audio_processor, training_parameters, model, device)
 
-# Initiating quantization process: making the model quantization aware
-#quantized_model = nemo.transform.quantize_pact(deepcopy(model), dummy_input=torch.randn((1,1,49,10)).to(device))
+    # Removing stored inputs and activations
+    print("Removing stored inputs and activations...")
+    remove_txt()
 
+    # Start training
+    print("Starting training...")
+    start = time.clock_gettime(0)
+    trainining_environment.train(model)
+    print('Finished Training on GPU in {:.2f} seconds'.format(time.clock_gettime(0) - start))
+
+if STEP_DO_EXPORT_MODEL:
+    # Load pretrained weights
+    model.load_state_dict(torch.load(CHECKPOINT_PATH, map_location=device))
+    
+    # first_layer_weights = model.conv1.weight
+
+    # 
+    # print("Shape of first layer weights:", first_layer_weights.shape)
+
+    # 
+    # print("First kernel weights (first filter) in the first layer:")
+    # print(first_layer_weights[1].detach().cpu().numpy())  
+
+    
+
+    path = os.path.join(".", EXPORT_OUTPUT_DIR_PATH)
+
+    # Remove the specified
+    # file path
+    if os.path.isdir(EXPORT_OUTPUT_DIR_PATH):
+        print("")
+    else:
+        os.mkdir(path)
+
+
+    try:
+        with open(EXPORT_OUTPUT_PATH, mode='w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+
+            
+            writer.writerow(["Layer Name", "Parameter Name", "Shape", "Values"])
+
+            
+            for name, param in model.named_parameters():
+                if param.requires_grad:
+                    param_name = name
+                    param_shape = list(param.shape)
+                    param_values = param.detach().cpu().numpy().flatten().tolist()
+                    
+                    # Write parameter details to CSV
+                    writer.writerow([name, param_name, param_shape, param_values])
+
+        print(f"Model parameters exported successfully to {EXPORT_OUTPUT_PATH}.")
+    except Exception as e:
+        print(f"Error exporting model parameters: {e}")
--- quantization/model.py
+++ quantization/model.py
@@ -20,7 +20,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from utils import npy_to_txt
 
 
